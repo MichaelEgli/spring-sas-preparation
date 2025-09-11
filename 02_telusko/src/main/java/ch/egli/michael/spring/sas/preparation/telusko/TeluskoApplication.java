ae83bff03f20805b88a7f8e7189ebeafd14901e3
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2025-09-11 15:15:38 +0200

    add 02_telusko

---
+++ 02_telusko/src/main/java/ch/egli/michael/spring/sas/preparation/telusko/TeluskoApplication.java
@@ -0,0 +1,13 @@
+package ch.egli.michael.spring.sas.preparation.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluskoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeluskoApplication.class, args);
+	}
+
+}
---
+++ 02_telusko/src/test/java/ch/egli/michael/spring/preparation/telusko/TeluskoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.egli.michael.spring.sas.preparation.telusko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeluskoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
