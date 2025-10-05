3aea6cdc4d56e569eb82b7b9a12a1078da0d3e77
MichaelEgli <michaelegli1879@gmail.com>
2025-10-05 12:54:31 +0200

    add controller, repo and service classes

---
+++ 06_ecom/src/main/java/com/telusko/ecom/repo/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.telusko.ecom.repo;
+
+import com.telusko.ecom.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
---
+++ 06_ecom/src/main/java/com/telusko/ecom/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.telusko.ecom.service;
+
+import com.telusko.ecom.model.Product;
+import com.telusko.ecom.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+}
