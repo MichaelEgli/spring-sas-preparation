ca38a7feee334a1140688244db6fe52ad7164bf9
MichaelEgli <michaelegli1879@gmail.com>
2025-10-05 13:32:16 +0200

    add constructors otherwise no data is shown in browser or postman even data is in h2 - lombok issue

--- 06_ecom/src/main/java/com/telusko/ecom/model/Product.java
+++ 06_ecom/src/main/java/com/telusko/ecom/model/Product.java
@@ -29,5 +29,89 @@ public class Product {
     private boolean available;
     private int quantity;
 
+    public Product() {}
 
+    public Product(int id, String name, String desc, String brand, BigDecimal price, String category, Date releaseDate, boolean available, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.brand = brand;
+        this.price = price;
+        this.category = category;
+        this.releaseDate = releaseDate;
+        this.available = available;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
