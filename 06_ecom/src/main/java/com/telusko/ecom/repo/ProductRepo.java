c9c3f7b2709b48dd6f3f10b7fcbae5f1cd42b472
MichaelEgli <michaelegli1879@gmail.com>
2025-10-05 13:20:22 +0200

    add data

--- 06_ecom/src/main/java/com/telusko/ecom/repo/ProductRepo.java
+++ 06_ecom/src/main/java/com/telusko/ecom/repo/ProductRepo.java
@@ -2,8 +2,6 @@ package com.telusko.ecom.repo;
 
 import com.telusko.ecom.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 }
