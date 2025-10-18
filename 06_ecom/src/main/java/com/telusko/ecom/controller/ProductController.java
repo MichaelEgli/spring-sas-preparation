c543276af86e755b6df035fad8771c18c3c4c81e
MichaelEgli <michaelegli1879@gmail.com>
2025-10-18 11:51:41 +0200

    add product works

--- 06_ecom/src/main/java/com/telusko/ecom/controller/ProductController.java
+++ 06_ecom/src/main/java/com/telusko/ecom/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -37,6 +38,21 @@ public class ProductController {
 
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 404 in postman (instead of 500)
+
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
+
+        try {
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
 
     }
 
